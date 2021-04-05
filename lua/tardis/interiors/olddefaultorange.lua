f671424777f72b2af6e89be1dd95f8cdd4a7abbc
Matt Jeanes <matthew.jeanes@myairops.com>
2021-04-05 19:14:53 +0100

    Add old default (orange)

---
+++ lua/tardis/interiors/olddefaultorange.lua
@@ -0,0 +1,16 @@
+-- Old Default (Orange)
+
+local T={}
+T.Base="olddefault"
+T.Name="Old Default (Orange)"
+T.ID="olddefaultorange"
+T.Interior={
+	Light={
+		color=Color(255,20,0),
+		warncolor=Color(255,100,0),
+		pos=Vector(0,0,0),
+		brightness=9.5
+	},
+}
+
+TARDIS:AddInterior(T)
